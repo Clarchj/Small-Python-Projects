e6c13bf4fd72db91025b942242ad15539097cfaa
Clarchj <117249851+Clarchj@users.noreply.github.com>
2024-08-23 23:38:53 +0700

    "new update"

---
+++ 100 days Python/main.py
@@ -0,0 +1,16 @@
+import another_module
+from turtle import Turtle,Screen
+
+print(another_module.another_function)
+
+timmy = Turtle()
+
+print (timmy)
+
+timmy.shape("turtle")
+timmy.speed(6)
+timmy.circle(120,180)
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
