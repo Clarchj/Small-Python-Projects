c7139806c078bdb535428e2eeb76a0ff87c24084
Clarchj <longcaoarch@gmail.com>
2024-09-12 13:32:20 +0700

    "initial to-do loop"

---
+++ portfolio projects/To-do App/main.py
@@ -0,0 +1,20 @@
+
+todos = []
+
+while True:
+                user_action = input('Type add or show:')
+                user_action = user_action.strip()
+
+                match user_action:
+                        case 'add':
+                                todo = input("enter a todo: ")
+                                todos.append(todo)
+                        case 'show':
+                                for item in todos:
+                                        print(item)
+                        case 'exit':
+                                break
+                        case _:
+                                print('unknown command')
+print("Bye!")
+
